c330f1f09042e11976cfbb3128ede20421552892
Sandeep Singh <sandeepgaurdec13@gmail.com>
2021-01-06 02:35:56 +0530

    ADDED:new contact address of the employee

--- AddressBook.java
+++ AddressBook.java
@@ -1,59 +1,72 @@
+
 /**
  * @author Sandeep_Singh
- * functionality Create contact and also print that contact
+ * functionality Add new Contact Address
  *
  */
+import java.util.Scanner;
+
+class Address {
+	private String city;
+	private String state;
+	private String zipcode;
+	private String email;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book Program");
+
+	}
+
+	public Address(String city, String state, String zipcode, String email) {
+		this.city = city;
+		this.state = state;
+		this.email = email;
+		this.zipcode = zipcode;
+
+	}
+
+	public String toString() {
+		return " [city=" + city + ", state=" + state + ", zipcode=" + zipcode + ", email=" + email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public String getemail() {
+		return email;
+	}
+
+}
+
 public class AddressBook {
-          private String city;
-          private String state;
-          private String zipcode;
-          private String email;
+	private static void addContact() {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("\nAddress:");
+		System.out.println("Enter the city:");
+		String city = inp.nextLine();
+		System.out.println("Enter the state:");
+		String state = inp.nextLine();
+		System.out.println("Enter the zipcode:");
+		String zipCode = inp.nextLine();
+		System.out.println("Enter the email-id:");
+		String email = inp.nextLine();
+		Address personAddress = new Address(city, state, zipCode, email);
+		System.out.println(personAddress);
+		inp.close();
+	}
 
-
-        public static void main(String[] args) {
-                System.out.println("Welcome To Address Book Program");
-                AddressBook obj1=new AddressBook("Mahoba","Uttar-Pradesh","210433","sandeepgaurdec13@gmail.com");
-                 
-                 String mycity=obj1.getCity();
-                 System.out.println(mycity);
-                 String mystate=obj1.getState();
-                 System.out.println(mystate);
-                 String myzipcode=obj1.getZipcode();
-                 System.out.println(myzipcode);
-                 String myemail=obj1.getemail();
-                 System.out.println(myemail);
-                 
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		addContact();
+	}
 }
-
-        public AddressBook(String city, String state, String zipcode, String email) {
-        this.city=city;
-        this.state=state;
-        this.email=email;
-        this.zipcode=zipcode;
-
-        }
-
-        public String toString() {
-                return " [city=" + city + ", state=" + state + ", zipcode=" + zipcode + ", email=" + email ;
-        }
-
-        public String getCity() {
-                return city;
-        }
-
-        public String getState() {
-                return state;
-        }
-
-        public String getZipcode() {
-                return zipcode;
-        }
-
-        public String getemail() {
-                return email;
-        }
-
-
-}
-
 
