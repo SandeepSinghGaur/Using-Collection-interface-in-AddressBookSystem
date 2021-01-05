d431f018bb22e8c0c93630d28579bf43e3ae8865
Sandeep Singh <sandeepgaurdec13@gmail.com>
2021-01-06 01:39:54 +0530

    Main Branch Simply Print Welcome Message

---
+++ AddressBook.java
@@ -0,0 +1,11 @@
+/**
+ * @author Sandeep_Singh 
+ * functionality print Welcome Message
+ *
+ */
+public class AddressBook {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book Program");
+	}
+}
+
