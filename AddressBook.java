7bba8c1aef52bd4184d435dfac6490c0f42077a0
Sandeep Singh <sandeepgaurdec13@gmail.com>
2021-01-06 02:12:39 +0530

    ADDED:We created Employee Personal Information Method

--- AddressBook.java
+++ AddressBook.java
@@ -1,11 +1,59 @@
 /**
- * @author Sandeep_Singh 
- * functionality print Welcome Message
+ * @author Sandeep_Singh
+ * functionality Create contact and also print that contact
  *
  */
 public class AddressBook {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book Program");
-	}
+          private String city;
+          private String state;
+          private String zipcode;
+          private String email;
+
+
+        public static void main(String[] args) {
+                System.out.println("Welcome To Address Book Program");
+                AddressBook obj1=new AddressBook("Mahoba","Uttar-Pradesh","210433","sandeepgaurdec13@gmail.com");
+                 
+                 String mycity=obj1.getCity();
+                 System.out.println(mycity);
+                 String mystate=obj1.getState();
+                 System.out.println(mystate);
+                 String myzipcode=obj1.getZipcode();
+                 System.out.println(myzipcode);
+                 String myemail=obj1.getemail();
+                 System.out.println(myemail);
+                 
 }
+
+        public AddressBook(String city, String state, String zipcode, String email) {
+        this.city=city;
+        this.state=state;
+        this.email=email;
+        this.zipcode=zipcode;
+
+        }
+
+        public String toString() {
+                return " [city=" + city + ", state=" + state + ", zipcode=" + zipcode + ", email=" + email ;
+        }
+
+        public String getCity() {
+                return city;
+        }
+
+        public String getState() {
+                return state;
+        }
+
+        public String getZipcode() {
+                return zipcode;
+        }
+
+        public String getemail() {
+                return email;
+        }
+
+
+}
+
 
